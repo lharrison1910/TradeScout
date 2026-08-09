d3d43ef2bfd767731d3ce73f8bc76dbb38a0993b
lharrison1910 <lewis.harrison0806@gmail.com>
2026-08-09 11:26:28 +0100

    Delete apps directory
    not required anymore

--- apps/ui/src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import Homepage from "../pages/Homepage/Homepage";
-
-export const Route = createFileRoute("/")({
-  component: Homepage,
-});
